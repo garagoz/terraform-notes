d0c183bae149c0f50fed759b18fb21d1819e8ee5
garagoz <atorayew@gmail.com>
2025-02-28 23:39:43 +0500

    terraform notes updated

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,65 +1,173 @@
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
- tags = {
-   Name = "Project VPC"
- }
-
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
 }
 
-resource "aws_subnet" "public_subnets" {
- count             = length(var.public_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
- cidr_block        = element(var.public_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
+# Public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.public_subnet_cidrs)
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
 
- tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
-
+  tags = {
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-public-subnet"
+    Environment = "${var.environment}"
+  }
 }
 
- 
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  count                   = length(var.private_subnet_cidrs)
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = false
 
-resource "aws_subnet" "private_subnets" {
- count             = length(var.private_subnet_cidrs)
- vpc_id            = aws_vpc.main.id
- cidr_block        = element(var.private_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
-
- tags = {
-   Name = "Private Subnet ${count.index + 1}"
- }
+  tags = {
+    Name        = "${var.environment}-${element(var.availability_zones, count.index)}-private-subnet"
+    Environment = "${var.environment}"
+  }
 }
 
-resource "aws_internet_gateway" "igw" {
- vpc_id = aws_vpc.main.id
-
- tags = {
-   Name = "Project VPC IGW"
- }
-
+# Internet gateway
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name"        = "${var.environment}-igw"
+    "Environment" = var.environment
+  }
 }
 
-resource "aws_route_table" "second_rt" {
-
- vpc_id = aws_vpc.main.id
-
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.igw.id
- }
-
- tags = {
-   Name = "2nd Route Table"
- }
-
+# Elastic-IP (eip) for NAT
+resource "aws_eip" "nat_eip" {
+  # vpc        = true
+  domain = "vpc"
+  depends_on = [aws_internet_gateway.ig]
 }
 
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
+  tags = {
+    Name        = "nat-gateway-${var.environment}"
+    Environment = "${var.environment}"
+  }
+}
+
+# Routing tables to route traffic for Private Subnet
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.environment}-private-route-table"
+    Environment = "${var.environment}"
+  }
+}
+
+# Routing tables to route traffic for Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name        = "${var.environment}-public-route-table"
+    Environment = "${var.environment}"
+  }
+}
+
+# Route for Internet Gateway
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig.id
+}
+
+# Route for NAT Gateway
+resource "aws_route" "private_internet_gateway" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.nat.id
+}
+
+# Route table associations for both Public subnet and Private subnet
 resource "aws_route_table_association" "public_subnet_asso" {
-
- count = length(var.public_subnet_cidrs)
- subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
- route_table_id = aws_route_table.second_rt.id
-
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+
+
+
+
+
+
+
+
+# resource "aws_subnet" "public_subnets" {
+#  count             = length(var.public_subnet_cidrs)
+#  vpc_id            = aws_vpc.main.id
+#  cidr_block        = element(var.public_subnet_cidrs, count.index)
+#  availability_zone = element(var.azs, count.index)
+
+#  tags = {
+#    Name = "Public Subnet ${count.index + 1}"
+#  }
+
+# }
+
+# resource "aws_subnet" "private_subnets" {
+#  count             = length(var.private_subnet_cidrs)
+#  vpc_id            = aws_vpc.main.id
+#  cidr_block        = element(var.private_subnet_cidrs, count.index)
+#  availability_zone = element(var.azs, count.index)
+
+#  tags = {
+#    Name = "Private Subnet ${count.index + 1}"
+#  }
+# }
+
+# resource "aws_internet_gateway" "igw" {
+#  vpc_id = aws_vpc.main.id
+
+#  tags = {
+#    Name = "Project VPC IGW"
+#  }
+# }
+
+# resource "aws_route_table" "second_rt" {
+
+#  vpc_id = aws_vpc.main.id
+
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.igw.id
+#  }
+
+#  tags = {
+#    Name = "2nd Route Table"
+#  }
+# }
+
+# resource "aws_route_table_association" "public_subnet_asso" {
+
+#  count = length(var.public_subnet_cidrs)
+#  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+#  route_table_id = aws_route_table.second_rt.id
+
+# }
--- vars.tf
+++ vars.tf
@@ -2,22 +2,29 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "environment" {
+  default = "arslan"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "CIDR block of the vpc"
+}
+
 variable "public_subnet_cidrs" {
  type        = list(string)
  description = "Public Subnet CIDR values"
- default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+ default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-
- 
 
 variable "private_subnet_cidrs" {
  type        = list(string)
  description = "Private Subnet CIDR values"
- default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+ default     = ["10.0.4.0/24", "10.0.5.0/24"]
 }
 
-variable "azs" {
+variable "availability_zones" {
  type        = list(string)
  description = "Availability Zones"
- default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+ default     = ["us-east-1a", "us-east-1b"]
 }
