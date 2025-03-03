080eae9463a73294f6696a5156aeb3addbb7b5ff
garagoz <atorayew@gmail.com>
2025-03-03 10:44:56 +0500

    terraform notes updated

---
+++ aws-vpc-ec2-sg/vars.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type = string
+  default = "arslan"
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "CIDR block of the vpc"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24"]
+}
+
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["us-east-1a"]
+}
